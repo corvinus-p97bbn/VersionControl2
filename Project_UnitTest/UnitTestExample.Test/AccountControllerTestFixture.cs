e05f9247204c0e870c25b5b98bdf883ea5132d11
corvinus-p97bbn <79064254+corvinus-p97bbn@users.noreply.github.com>
2021-11-26 23:23:39 +0100

    Redo.

---
+++ Project_UnitTest/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTestExample.Test
+{
+    internal class AccountControllerTestFixture
+    {
+        [Test]
+
+        public void TestValidateEmail(string email, bool expectedResult)
+        { 
+        public string email { get; set; }
+        public bool expectedResult { get; set; }
+       }
+
+    }
+}
+
---
+++ Project_UnitTest/UnitTestExample.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestExample.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestExample.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("be3e7d51-628a-4c52-a903-3ed9d650f9a6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
