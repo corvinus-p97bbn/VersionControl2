1d22521d1d58294eeeb8ffd2ac15b2aea6cba979
corvinus-p97bbn <79064254+corvinus-p97bbn@users.noreply.github.com>
2021-12-03 23:52:58 +0100

    Don

---
+++ Project_Mikroszimuláció2/Project_Mikroszimuláció2/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Mikroszimuláció2.Entities
+{
+    internal class DeathProbability
+    {
+        public int kor { get; set; }
+        public int nem { get; set; }
+        public double halalozasvalosz { get; set; }
+    }
+}
